9dba54f040ec0388dc69158577f4a0c7863a318a
Colby Fayock <fayock@gmail.com>
2024-02-07 16:37:55 -0800

    video, context

--- src/app/destinations/[destinationId]/page.tsx
+++ src/app/destinations/[destinationId]/page.tsx
@@ -1,13 +1,15 @@
 import { redirect } from 'next/navigation';
-import { revalidatePath } from 'next/cache';
 import { getCldImageUrl } from 'next-cloudinary';
 import { v2 as cloudinary } from 'cloudinary';
 
 import Container from '@/components/Container';
 import CldImage from '@/components/CldImage';
+import CldVideoPlayer from '@/components/CldVideoPlayer';
 
 import destinations from '@/data/destinations.json';
 import CldUploadButton from '@/components/CldUploadButton';
+
+import 'next-cloudinary/dist/cld-video-player.css';
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
@@ -22,20 +24,9 @@ export default async function Destination({ params }: { params: { destinationId:
     redirect('/404');
   }
 
-  const results = await cloudinary.search.expression(`tags=traveler-photo`).execute();
+  const results = await cloudinary.search.expression(`tags=traveler-photo`).with_field('context').execute();
   const { resources: travelerPhotos } = results || {};
-  console.log('travelerPhotos', travelerPhotos)
-
-  async function onUpload(formData: FormData) {
-    'use server';
-    
-    console.log('destination', destination);
-    if ( !destination ) return;
-    console.log('`/destinations/${destination.id}`', `/destinations/${destination.id}`)
-    revalidatePath('/destinations/[destinationId]');
-    console.log('formData', formData)
-  }
-
+console.log(travelerPhotos)
   return (
     <>
       <Container className="relative flex max-w-7xl items-center justify-center aspect-[3/1] bg-black">
@@ -58,31 +49,46 @@ export default async function Destination({ params }: { params: { destinationId:
         </div>
       </Container>
       <Container className="mt-12">
+        <CldVideoPlayer
+          id="brazil"
+          width="640"
+          height="360"
+          src="travel-website/beach_-_78287_1080p_ihcgbj"
+          transformation={{
+            streaming_profile: 'hd',
+          }}
+          sourceTypes={['hls']}
+        />
+      </Container>
+      <Container className="mt-12">
         <div className="prose-lg mx-auto">
           <div className="flex justify-between items-center">
             <h2>Traveler&apos;s Photos</h2>
             <CldUploadButton
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              onUpload={onUpload}
               uploadPreset="travel-website"
               signatureEndpoint="/api/sign-cloudinary-params"
               options={{
-                tags: ['traveler-photo', `destination-${destination.id}`]
+                tags: ['traveler-photo', `destination-${destination.id}`],
+                detection: 'captioning',
+                on_success: 'current_asset.update({ tags: [...e.upload_info.tags, "autocaption"], context: {caption: e.upload_info?.info?.detection?.captioning?.data?.caption} })'
               }}
             >
               Add a Photo
             </CldUploadButton>
           </div>
           {Array.isArray(travelerPhotos) && travelerPhotos.length > 0 && (
-            <ul className="grid grid-cols-4">
+            <ul className="grid grid-cols-4 gap-2 p-0">
               { travelerPhotos.map(photo => {
                 return (
-                  <li key={photo.public_id}>
+                  <li key={photo.public_id} className="m-0 p-0">
                     <CldImage
+                      className="m-0"
                       src={photo.public_id}
-                      width={photo.width}
-                      height={photo.height}
-                      alt=""
+                      width={526}
+                      height={526}
+                      crop="fill"
+                      alt={photo.context?.caption}
                     />
                   </li>
                 )
--- src/components/CldUploadButton/CldUploadButton.tsx
+++ src/components/CldUploadButton/CldUploadButton.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import { CldUploadButton as CldUploadButtonDefault, CldUploadButtonProps, CldUploadWidgetResults } from 'next-cloudinary';
+import { CldUploadButton as CldUploadButtonDefault, CldUploadButtonProps } from 'next-cloudinary';
 
-interface CldUploadButtonPropsWithFormData extends Omit<CldUploadButtonProps, "onUpload"> {
-  onUpload: (formData: FormData) => void
-}
-
-const CldUploadButton = (props: CldUploadButtonPropsWithFormData) => {
-  async function handleOnUpload(results: CldUploadWidgetResults) {
-    if ( typeof props.onUpload === 'function' ) {
-      const formData = new FormData();
-      formData.append('results', JSON.stringify(results))
-      props.onUpload(formData);
-    }
-  }
-  return <CldUploadButtonDefault {...props} onUpload={handleOnUpload} />;
+const CldUploadButton = (props: CldUploadButtonProps) => {
+  return <CldUploadButtonDefault {...props} />;
 }
 
 export default CldUploadButton;
